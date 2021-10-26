6cf9e97d12fd6a0bf147bb7c2e7f724df5aa62e3
Tal-Nitzan <tal.nitzan91@gmail.com>
2021-10-26 21:14:20 +0300

    Commit test

--- app/src/main/java/com/example/enviromax/MainActivity.java
+++ app/src/main/java/com/example/enviromax/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
     private NavigationView nvDrawer;
-    private int x;
     private static int numOfActiveDevices = 0;
     private ArrayList<Device> deviceArrayList;
 
